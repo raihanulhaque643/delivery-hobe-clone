7c97432d3c5648d5ea4235809ec4a3c9a3730f43
Raihanul Haque <raihan.hd.500@gmail.com>
2021-04-26 15:19:21 +0600

    product ui changed

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,11 +2,23 @@ import React from 'react'
 
 const Product = ({ id, name, price, subcategory, inStock, image }) => {
     return (
-        <div className="h-44 w-auto md:h-52 rounded-xl flex justify-center items-end bg-auto bg-center bg-no-repeat text-white font-semibold my-4 bg-white"
-        style={{'backgroundImage' : `url(${image})`}}
-        >
-            <div className="bg-black w-full flex justify-center items-center h-8 bg-opacity-50 rounded-b-xl text-sm sm:text-md h-10 text-center">
+        <div className="w-auto md:h-auto rounded-xl flex justify-center items-center text-black font-semibold my-2 py-2 bg-white flex flex-col">
+            <img src={image} alt="" style={{'objectFit': 'contain', 'height': '140px', 'width': '140px'}}/>
+            <div className="flex flex-col w-full h-auto p-2">
+            <div className="w-full flex rounded-b-xl text-sm sm:text-md h-10">
             {name}
+            </div>
+            <div className="w-full rounded-b-xl text-sm sm:text-md h-10 text-gray-400">
+            {subcategory}
+            </div>
+            <div className="w-full rounded-b-xl text-sm sm:text-md h-10">
+            <button className="border w-full font-semibold p-2 rounded-md">
+                <div className="flex flex-row justify-between">
+                    <div className="">Buy</div>
+                    <div className="">৳ {price}</div>
+                </div>
+            </button>
+            </div>
             </div>
         </div>
     )
