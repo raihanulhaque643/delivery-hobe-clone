f362ca23912dfaa58ded21ad4d2e5166cd2a5ccb
Raihanul Haque <raihan.hd.500@gmail.com>
2021-04-29 15:40:57 +0600

    step components on homepage alingment center

--- src/components/StepThree.js
+++ src/components/StepThree.js
@@ -4,7 +4,7 @@ const StepThree = () => {
     return (
         <div className="flex justify-center items-center w-full bg-black py-4">
             <div className="w-80 flex flex-col items-center">
-                <div className="text-center bg-yellow-300 h-10 w-10 text-black font-bold text-2xl rounded-full my-2">3</div>
+                <div className="text-center bg-yellow-300 h-10 w-10 text-black font-bold text-2xl rounded-full my-2  flex justify-center items-center">3</div>
                 <div className="text-sm text-center my-2 font-semibold text-yellow-300">We deliver your products within 1 - 3 Hrs!</div>
                 <div className="text-sm text-center text-gray-500">Depending on traffic, product availability, order demand and weather conditions!</div>
             </div>
